cc42b386555be6109bb433f3061c4fa40ef366d1
fjktkm <fjktkm@gmail.com>
2024-04-19 09:24:33 +0000

    fix: デプロイの設定を変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://fjktkm.github.io',
+    site: 'https://fjktkm.com',
     base: '/astro-tutorial',
 });
