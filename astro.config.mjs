78cf899c80eab47c31ceaeb106588d21ddb2a329
fjktkm <fjktkm@gmail.com>
2024-04-21 13:00:05 +0000

    fix: サブディレクトリでデプロイするための設定を修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://fjktkm.com',
-    base: '/astro-tutorial/',
+    base: '/astro-tutorial',
 });
